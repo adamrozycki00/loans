5538f16677001dcaf77e88f75d8ec1993f00f8a0
Adam Rozycki <adamrozycki00@gmail.com>
2021-02-18 01:16:14 +0100

    Initial commit

---
+++ src/test/java/com/tenetmind/loans/LoansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenetmind.loans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
