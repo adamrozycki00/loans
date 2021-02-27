8d4f66846745371d62cd77bf29ce1cce9e04f02f
adamrozycki00 <adamrozycki00@gmail.com>
2021-02-27 15:54:15 +0100

    Refactor PaymentDto

--- src/main/java/com/tenetmind/loans/operation/service/PaymentDto.java
+++ src/main/java/com/tenetmind/loans/operation/service/PaymentDto.java
@@ -2,18 +2,22 @@ package com.tenetmind.loans.operation.service;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
+@Setter
 public class PaymentDto {
 
-    private final LocalDate date;
-    private final Long loanId;
-    private final String currencyName;
-    private final BigDecimal amount;
+    private LocalDate date;
+    private Long loanId;
+    private String currencyName;
+    private BigDecimal amount;
 
     public PaymentDto(LocalDate date, Long loanId) {
         this.date = date;
